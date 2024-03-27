0aa9e78a2852e09426c1d79ba215b9bec4c9ddc1
Fran-Lis <fl66@live.it>
2024-03-27 15:16:03 +0100

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,33 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import { Provider } from 'react-redux';
 import store from './store/store';
 import Form from './components/Form/Form';
 import ActDetail from './components/ActDetail/ActDetail';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  },
-  {
-    path: '/addNew',
-    element: <Form />
-  },
-  {
-    path: '/:activityId',
-    element: <ActDetail />
-  }
-])
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<App />} />
+          <Route exact path="/addNew" element={<Form />} />
+          <Route exact path="/:activityId" element={<ActDetail />} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
+
 
